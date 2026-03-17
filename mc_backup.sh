e5129a4e760d549166282ba87ed4e0332ddf82f2
Felix Gabel <113338967+felixgabel@users.noreply.github.com>
2026-03-17 20:58:24 +0100

    Refactor mc_backup.sh for clarity and simplicity
    Removed comments in German and simplified the final message.

--- mc_backup.sh
+++ mc_backup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Minecraft Backup Script
-# Kopiert ausgewählte Minecraft-Dateien und -Ordner auf den Desktop
-
 SRC='/Users/felix/Library/Application Support/minecraft'
 DATE=$(date +"%Y_%m_%d")
 DST="/Users/felix/Desktop/$DATE"
@@ -27,31 +24,35 @@ echo "from: $SRC"
 echo "to: $DST"
 echo ""
 
-# Verzeichnisse kopieren
+# copy directories
 for dir in "${DIRS[@]}"; do
     SRC_PATH="$SRC/$dir"
     if [ -d "$SRC_PATH" ]; then
         echo "copying $dir/..."
         cp -R "$SRC_PATH" "$DST/"
         echo "done"
+        echo ""
     else
         echo "'$dir' not found! proceed with next..."
+        echo ""
     fi
 done
 
 echo ""
 
-# Einzelne Dateien kopieren
+# copy files
 for file in "${FILES[@]}"; do
     SRC_PATH="$SRC/$file"
     if [ -f "$SRC_PATH" ]; then
         echo "copying $file..."
         cp "$SRC_PATH" "$DST/"
         echo "done"
+        echo ""
     else
         echo "'$file' not found! proceed with next..."
+        echo ""
     fi
 done
 
 echo ""
-echo "backup finished!"
+echo "finished"
